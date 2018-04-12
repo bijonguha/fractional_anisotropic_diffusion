6d6544d6d551a69df5d0feea018fe0bf0f72b450
bijonguha <iitbguha@gmail.com>
2018-04-12 13:03:26 +0530

    anisodiff.m recorrected, main_fun.m me se median filtering hataya, wave_denoising se level wale parameter ko correct kia

--- main_fun.m
+++ main_fun.m
@@ -8,39 +8,41 @@ if ndims(ig)==3
   ig = rgb2gray(ig);
 end
 
-im = imnoise(ig,'gaussian',0,0.01);
-[psnr(uint8(im),uint8(ig))]
+imN = imnoise(ig,'gaussian',0,0.03);
+[psnr(uint8(imN),uint8(ig))]
 
-[wA,wH,wV,wD] = dwt2(im,'db4');
+im = wave_denoising(imN,'sym4','s',1);
+
+[wA,wH,wV,wD] = dwt2(im,'sym4');
 wA = im2double(wA);
 wH = im2double(wH);
 wV = im2double(wV);
 wD = im2double(wD);
 
-imgHi = idwt2(wA,wH,zeros(size(wV)),zeros(size(wD)),'db4');
-imgH = wave_denoising(imgHi,'db4','s',1);
-imgVi = idwt2(wA,zeros(size(wH)),wV,zeros(size(wD)),'db4');
-imgV = wave_denoising(imgVi,'db4','s',1);
-imgDi = idwt2(wA,zeros(size(wH)),zeros(size(wV)),wD,'db4');
-imgD = wave_denoising(imgDi,'db4','s',1);
+imgH = idwt2(wA,wH,zeros(size(wV)),zeros(size(wD)),'sym4');
+%imgH = wave_denoising(imgHi,'db4','s',1);
+imgV = idwt2(wA,zeros(size(wH)),wV,zeros(size(wD)),'sym4');
+%imgV = wave_denoising(imgVi,'db4','s',1);
+imgD = idwt2(wA,zeros(size(wH)),zeros(size(wV)),wD,'sym4');
+%imgD = wave_denoising(imgDi,'db4','s',1);
 
-resALL = medfilt2(fanisodiff(im,14,20,0.30,2,1.1,'AL4'),[5,5]);
-resH = medfilt2(fanisodiff(imgH,10,20,0.30,2,1.1,'NS'),[5,1]);
-resV = medfilt2(fanisodiff(imgV,10,20,0.30,2,1.1,'EW'),[1,5]);
-resD = medfilt2(fanisodiff(imgD,12,20,0.30,2,1,'D'),[5,5]);
+resALL = wave_denoising(fanisodiff(im,8,20,0.30,2,1.1,'AL4'),'sym4','s',4);
+resH = wave_denoising(fanisodiff(imgH,8,20,0.30,2,1.1,'NS'),'sym4','s',1);
+resV = wave_denoising(fanisodiff(imgV,8,20,0.30,2,1.1,'EW'),'sym4','s',1);
+resD = wave_denoising(fanisodiff(imgD,5,20,0.30,2,1,'D'),'sym4','s',1);
 
 img = (resALL+0.4*resH+0.4*resV+0.2*resD)/2;
-img = fanisodiff(img,2,20,0.20,1,1,'AL4');
+img = fanisodiff(img,5,20,0.20,1,1,'AL4');
 
 figure()
 subplot(2,2,2)
 imshow(uint8(img));title('Proposed');
 subplot(2,2,3)
-imshow(uint8(anisodiff(im,18,25,0.30,2)));title('P-M Model');
+imshow(uint8(anisodiff(imN,16,25,0.25,2)));title('P-M Model');
 subplot(2,2,4)
-imshow(uint8(wave_denoising(im,'haar',2)));title('Wavelet Thresholding');
+imshow(uint8(wave_denoising(imN,'db4','s',2)));title('Wavelet Thresholding');
 subplot(2,2,1)
-imshow(uint8(im));title('Nosiy Image');
+imshow(uint8(imN));title('Nosiy Image');
 
-[psnr(uint8(img),uint8(ig)),psnr(uint8(anisodiff(im,13,25,0.30,2)),uint8(ig)),...
-    psnr(uint8(wave_denoising(im,'haar',2)),uint8(ig))]
+[psnr(uint8(img),uint8(ig)),psnr(uint8(anisodiff(imN,16,25,0.25,2)),uint8(ig)),...
+    psnr(uint8(wave_denoising(imN,'db4','s',2)),uint8(ig))]
--- wave_denoising.m
+++ wave_denoising.m
@@ -2,6 +2,5 @@ function w_thresh = wave_denoising(g, wlt, sorh,level)
 x = uint8(g);
 [thr,sorh,keepapp] = ddencmp('den','wv',x);
 sorh = 's';
-level = 1;
 w_thresh = wdencmp('gbl',x,wlt,level,thr,sorh,keepapp);
 end
